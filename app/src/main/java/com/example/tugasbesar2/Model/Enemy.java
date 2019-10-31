fbcf2b1c368ab7ca9f2e8570134627123753c69d
Kevin Draven <kevindravenk@gmail.com>
2019-10-31 07:55:11 +0700

    [ADD]GameFragment, dll

--- app/src/main/java/com/example/tugasbesar2/Model/Enemy.java
+++ app/src/main/java/com/example/tugasbesar2/Model/Enemy.java
@@ -3,10 +3,14 @@ package com.example.tugasbesar2.Model;
 public class Enemy {
     private float posX;
     private float posY;
+    private float ukuranX;
+    private float ukuranY;
 
-    public Enemy(float x, float y){
+    public Enemy(float x, float y, float ukuranX, float ukuranY){
         this.posX = x;
         this.posY = y;
+        this.ukuranX = ukuranX;
+        this.ukuranY = ukuranY;
     }
 
     void moveHorizontal(float x){
