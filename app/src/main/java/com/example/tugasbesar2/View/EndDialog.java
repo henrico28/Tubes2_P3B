2c872aeed26872cc91cf790a5cf73bca0ef1ff1e
kelvindravin <kelvindravin@users.noreply.github.com>
2019-11-03 13:04:42 +0700

    [UPDATE-Scoreboard/Restart] +Score is now exist +Can Restart app when game over

--- app/src/main/java/com/example/tugasbesar2/View/EndDialog.java
+++ app/src/main/java/com/example/tugasbesar2/View/EndDialog.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ import com.example.tugasbesar2.R;
 
 public class EndDialog extends DialogFragment {
     private TextView tv_score;
+    private Button button;
 
     @Nullable
     @Override
@@ -22,6 +24,14 @@ public class EndDialog extends DialogFragment {
         this.tv_score = view.findViewById(R.id.tv_your_score);
         String akhir = "Your Score : "+this.getTag();
         this.tv_score.setText(akhir);
+        this.button = view.findViewById(R.id.btn_restart);
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().recreate();
+                dismiss();
+            }
+        });
         return view;
     }
 
